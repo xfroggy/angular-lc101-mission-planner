2dab58b86b97e56040b30b43f0ea66d8035120dd
Todd Smitala <tc.smitala@gmail.com>
2020-11-16 14:29:10 -0500

    completed Part I

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -21,11 +21,43 @@ export class EquipmentComponent implements OnInit {
    cargoMass: number = 0;
    maximumAllowedMass: number = 2000;
    maxItems: number = 10;
+   nearMax: boolean = false;
+   remainingBudget: number = 2000;
 
    constructor() { }
 
    ngOnInit() { }
 
    // Code your addItem function here:
+   addItem(equipment: object, weight: number){        
+     this.cargoHold.push(equipment);
+     this.cargoMass = this.cargoMass + weight;
+     this.remainingBudget -= weight;
+     if (this.cargoMass > (this.maximumAllowedMass-200) && this.cargoMass <=this.maximumAllowedMass){
+       this.nearMax = true;
+       return this.nearMax;
+     }else return false;
+   }
+
+   checkItems(weight: number){
+     if (this.cargoHold.length === this.maxItems){
+      return true;
+     }else {
+       if ((this.cargoMass + weight)> this.maximumAllowedMass){
+       return true;
+      }  
+     } 
+      return false;
+   }
+
+   emptyHold(){
+     this.nearMax = false;
+     this.cargoHold = [];
+     this.cargoMass = 0;
+     this.remainingBudget = 2000;
+     
+   }
+
+
    
 }
